1b81a3e110e91d52868e697eaced2de29d7cee8f
Daniel Minerva <dminerva94@yahoo.com>
2018-04-04 14:12:03 -0400

    finished section 3

---
+++ 3. An Informal Introduction to Python/3.1. Using Python as a Calculator/Lists.py
@@ -0,0 +1,8 @@
+squares = [1, 4, 9, 16, 25]
+print(squares)
+print(squares[0])  # indexing returns the item
+print(squares[-1])
+print(squares + [36, 49, 64, 81, 100])
+squares.append(89) # You can also add new items at the end of the list, by using the append() method
+print(squares)
+
--- 3. An Informal Introduction to Python/3.1. Using Python as a Calculator/Strings.py
+++ 3. An Informal Introduction to Python/3.1. Using Python as a Calculator/Strings.py
@@ -18,3 +18,5 @@ print(word[5])  # character in position 5
 print(word[-1])  # last characte
 print(word[-2])  # second-last character
 print(word[0:2])  # characters from position 0 (included) to 2 (excluded)
+print(word[:2] + word[2:])
+print(len(word))
